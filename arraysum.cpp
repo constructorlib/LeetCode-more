3923295be55aab7b81dfb9c43a3788a41e56b242
lugburz <lugburzlib@gmail.com>
2022-09-22 16:19:54 +0900

    HackerRank

---
+++ arraysum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+Algorithms problem 1, sum up the array.
+*/
+int main() {
+    int e{0}; 
+    cin >> e;
+    int arr[e]{};
+    int sum{0};
+    for (int i = 0; i < e; i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    // for (int i = 0; i < e; i++){
+    //     cin >> arr[i];
+    // }
+
+    // for (int i = 0; i < e; i++){
+    //     sum = sum + arr[i];
+    // }
+    // cout << (sum);
+    return 0;
+}
